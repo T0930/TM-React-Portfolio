c1726e24a9b274b62d7a0d30726b375f296c5844
Tom M <T0930@protonmail.com>
2022-02-17 23:03:57 -0700

    updated readme and Home.js portfolio photo

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import profpic from "../../images/profilepic.gif";
+
 
 
 export default function Home() {
   return (
     <header className="masthead bg-primary text-white text-center homeFormat">
     <div className="container d-flex align-items-center flex-column">
-        <img className="masthead-avatar mb-5 photoEdit" src="/images/profilepic.gif" alt=""/>
+        <img className="masthead-avatar mb-5 photoEdit" src={profpic} alt=""/>
         <h1 className="masthead-heading mb-0">TOM MYLASTNAME</h1>
         <div className="divider-custom divider-light">
         </div>
